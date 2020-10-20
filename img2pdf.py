7ea6e3c3ffdea1bbd3ef5f901f4a1a7d728f7d65
Michael Melikov <coutaq@gmail.com>
2020-10-20 11:55:52 +0300

    and fixed a bug

--- img2pdf.py
+++ img2pdf.py
@@ -15,8 +15,7 @@ import os
 botToken = str(sys.argv[1])
 updater = Updater(token=botToken, use_context=True)
 bot = Bot(token=botToken)
-print("{}:Bot started.".format(datetime.now())
-photos = deque()
+print("{}:Bot started.".format(datetime.now()))
 dispatcher = updater.dispatcher
 pdfs = {}
 
