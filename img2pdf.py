4357e149faef8d38efa430cae0c29eaaec978d9f
Michael Melikov <coutaq@gmail.com>
2020-11-03 03:45:39 +0300

    added the ability to add spaces to filenames

--- img2pdf.py
+++ img2pdf.py
@@ -36,10 +36,14 @@ def unknown(update, context):
 def upload(update, context):
     user = update.message.from_user
     chat = update.effective_chat.id
+    author = "{} {}".format(user.first_name if user.language_code=="en" else user.last_name, user.last_name if user.language_code=="en" else user.first_name)
+    filename = ""
     if(not context.args):
         context.args = ["{}.pdf".format(user.username)]
-    filename = context.args[0]
-    pdfs[chat] = PDF(chat, user.username, user.language_code, filename)
+    for word in context.args:
+        filename+=" "+word
+    filename.strip()
+    pdfs[chat] = PDF(chat, user.username, user.language_code, filename, author)
     context.bot.send_message(chat_id=chat, text=getLocalized("upload", user.language_code))
 
 def getPhoto(update, context):
@@ -52,10 +56,17 @@ def getFile(update, context):
 
 def create(update, context):
     chat = update.effective_chat.id
-    pdf = pdfs[chat]
-    pdf.createPFD()
-    pdf.uploadPDF()
-    pdfs.pop(chat)
+    try:
+        pdf = pdfs[chat]
+        if pdf.isEmpty():
+            context.bot.send_message(chat_id=chat, text=getLocalized("pdfEmptyError", update.message.from_user.language_code))
+        else:
+            pdf.createPFD()
+            pdf.uploadPDF()
+            pdfs.pop(chat)
+    except KeyError:
+        context.bot.send_message(chat_id=chat, text=getLocalized("fileNotInitiatedError", update.message.from_user.language_code))
+        pass
 
 dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(CommandHandler('help', help))
@@ -68,7 +79,7 @@ dispatcher.add_handler(MessageHandler(Filters.document.category("image") & (~Fil
 updater.start_polling()
 
 class PDF:
-    def __init__(self, chat_id, user_id, lc, filename):
+    def __init__(self, chat_id, user_id, lc, filename, author):
         self.chat_id = chat_id
         self.user_id = user_id
         self.lc = lc
@@ -76,6 +87,7 @@ class PDF:
             filename+=".pdf"
         self.filename = filename
         self.images = deque()
+        self.author = author
         logToConsole("User @{}(chat_id:{}) created {}.".format(user_id, chat_id, filename))
 
     def append(self, image):
@@ -83,8 +95,10 @@ class PDF:
         self.images.append(image)
 
     def createPFD(self):
-        logToConsole("User @{}(chat_id:{}) uploaded and combined the pictures into {}.".format(self.chat_id, self.user_id, self.filename))
+        logToConsole("User @{}(chat_id:{}) uploaded and combined the pictures into {}.".format(self.user_id, self.chat_id, self.filename))
         canvas = Canvas(filename=self.filename)
+        canvas.setTitle(self.filename)
+        canvas.setAuthor(self.author)
         for image in self.images:
             bytes = BytesIO(bot.getFile(image).download_as_bytearray())  
             page = Image.open(bytes).convert("RGBA")
@@ -101,11 +115,22 @@ class PDF:
 
     def uploadPDF(self):
         file = open(self.filename, 'rb')
-        logToConsole("User @{}(chat_id:{}) got theirs pdf {}.".format(self.chat_id, self.user_id, self.filename))
+        logToConsole("User @{}(chat_id:{})'s pdf {} was succesfully created.".format(self.user_id, self.chat_id, self.filename))
         bot.send_message(chat_id=self.chat_id, text=getLocalized("sending", self.lc))
-        bot.send_document(chat_id=self.chat_id, document=file)
+        for i in range(10):
+            try:
+                bot.send_document(chat_id=self.chat_id, document=file)
+                break
+            except Exception as e:
+                bot.send_message(chat_id=self.chat_id, text=getLocalized("uploadingError", self.lc))
+                logToConsole("User's @{}(chat_id:{}) pdf {} was not uploaded({}/10) because of an Exception({}).".format(self.user_id, self.chat_id, self.filename, i, e.__class__))
         file.close()
+        logToConsole("User @{}(chat_id:{}) got theirs pdf {}.".format(self.user_id, self.chat_id, self.filename))
         os.remove(self.filename)
+
+    def isEmpty(self):
+        print(self.images.count)
+        return self.images.count==0
 
 
 localizedStrings = {
@@ -114,14 +139,30 @@ localizedStrings = {
         "upload" : "Upload images up to 20mb per image.\nWhen you're done use /create to create the pdf",
         "success": "Photo succesfully uploaded!",
         "sending": "Uploading the pdf!",
-        "unknown": "Sorry, I didn't understand that command."
+        "unknown": "Sorry, I didn't understand that command.",
+        "fileNotInitiatedError" : "You must first use the /upload command before using /create.",
+        "pdfEmptyError": "You cannot create an empty pdf!",
+        "uploadingError": "Upload failed!"
   },
     "ru" : {
         "start" : "Бот для создания pdf из картинок.\nЧтобы начать, используйте комманду\n/upload <Название>.\nMade by @coutaq.",
         "upload" : "Загрузите картинки.\nКогда загрузите, используйте /create",
         "success": "Фото загружено!",
         "sending": "Отправляю .pdf!",
-        "unknown": "Извините, я такое не умею."
+        "unknown": "Извините, я такое не умею.",
+        "fileNotInitiatedError" : "Перед использованием этой команды, нужно создать файл с помощью /upload.",
+        "pdfEmptyError": "Нельзя создать пустой документ!",
+        "uploadingError": "Не удалось отправить документ!"
+  },
+   "uk" : {
+        "start" : "Бот для створення pdf з картинок.\nДля того щоб розпочати, використовуйте комманду\n/upload <Назва>.\nMade by @coutaq, translated by @Imllush.",
+        "upload" : "Завантажте картинки.\nКоли завантажити, використовуйте /create",
+        "success": "Картинка завантажена!",
+        "sending": "Відправляю .pdf!",
+        "unknown": "Вибачте, я таке не вмію.",
+        "fileNotInitiatedError" : "Перед використанням цієї команди, потрібно створити файл за допомогою /upload.",
+        "pdfEmptyError": "Неможливо створити пустий документ!",
+        "uploadingError": "Не вдалося відправити документ!"
   },
 }
 def getLocalized(string, lc):
@@ -130,12 +171,3 @@ def getLocalized(string, lc):
     else:
         dictionary = localizedStrings.get("en")
     return dictionary.get(string)
-
-
-
-# def signal_handler(signal, frame):
-#     updater.idle()
-#     logToConsole("Bot stopped by keyboard interrupt.")  
-#     sys.exit(0)
-
-# signal.signal(signal.SIGINT, signal_handler)
